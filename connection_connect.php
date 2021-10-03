95d25d4e5e6187a750b34974366ec5e8b6972e7e
Baifernnn <panalai254129@gmail.com>
2021-10-04 02:01:30 +0700

    Create connection_connect.php

---
+++ connection_connect.php
@@ -0,0 +1,13 @@
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "databaseName";
+$conn = new mysqli($servername,$username,$password);
+if ($conn->connect_error) {
+    die("Connection failed:" .$conn->connect_error);
+}
+if (!$conn->select_db($dbname)){
+    die("Connection failed:" .$conn->connect_error);
+}
+
+ 

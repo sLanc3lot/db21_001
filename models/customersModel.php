135927b2389f8090a1883bb6604dee449b02676b
wuttitul <91468323+wuttitul@users.noreply.github.com>
2021-10-04 15:51:23 +0700

    Update customersModel.php

--- models/customersModel.php
+++ models/customersModel.php
@@ -32,13 +32,5 @@
             require("connection_close.php");
             return $customersList;
         }
-
-        public static function add($ID,$code,$name,$address,$tel){
-            require("connection_connect.php");
-            $sql = "INSERT INTO customers(ID,code,name,address,tel) value ('$ID','$code','$name','$address','$tel')"
-            $result=conn->query($sql);
-            require("connection_close.php");
-            return "add success $result rows";
-        }
     }
 ?>

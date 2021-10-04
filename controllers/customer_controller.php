96c28da2f6151538f91b4298e9072aab68d28f5b
wuttitul <91468323+wuttitul@users.noreply.github.com>
2021-10-04 13:05:50 +0700

    Update customer_controller.php

--- controllers/customer_controller.php
+++ controllers/customer_controller.php
@@ -1,5 +1,5 @@
 <?php
-class customerController{
+class customersController{
     public function index(){
         $customer_list = customers::getAll();
         require_once('views/customers/index_customers.php');

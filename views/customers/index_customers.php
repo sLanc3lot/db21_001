199abc533549dc71c0391852f27a28f244c671e7
wuttitul <91468323+wuttitul@users.noreply.github.com>
2021-10-04 12:57:22 +0700

    Create index_customers.php

---
+++ views/customers/index_customers.php
@@ -0,0 +1,14 @@
+<table border =1>
+<tr> <td>ID</td><td>code</td><td>name</td><td>addredd</td><td>tel</td>
+<td>Department</td><td>update</td><td>delete</td> </tr>
+
+
+<?php foreach($customers_list as $customers){
+    echo "<tr> <td>$customers->ID</td>
+    <td>$customers->code</td> <td>$customers->name</td>
+    <td>$customers->address</td> <td>$customers->tel</td>
+    <td>$customers->depName</td> <td>$update</td> <td>delete</td>;"
+}
+echo "</table>";
+
+?>

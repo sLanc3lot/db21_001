6fa0391c2d196f0522dbedbfa7038b10c290fdc4
wuttitul <91468323+wuttitul@users.noreply.github.com>
2021-10-04 14:24:24 +0700

    Create product_colorModel.php

---
+++ models/product_colorModel.php
@@ -0,0 +1,13 @@
+<?php
+class product_color{
+    public $C_ID:
+    public $PR_CODE;
+    public $PR_COLOR;
+
+    public function_construct($C_ID,$PR_CODE,$PR_COLOR){
+        $this->C_ID=$C_ID;
+        $this->PR_CODE=$PR_CODE;
+        $this->PR_COLOR=$PR_COLOR;
+    }
+}
+?>

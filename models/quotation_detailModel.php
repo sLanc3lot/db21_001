35b3c0f493d13575076cfb0cc6fe50fb4072f5ea
wuttitul <91468323+wuttitul@users.noreply.github.com>
2021-10-04 16:13:52 +0700

    Update quotation_detailModel.php

--- models/quotation_detailModel.php
+++ models/quotation_detailModel.php
@@ -14,7 +14,23 @@
             $this->QUD_AMOUNT=$QUD_AMOUNT;
             $this->QUD_PRINT=$QUD_PRINT;
         }
-
+        
+        public static function getAll(){
+            $quotation_detailList=[];
+            require("connection_connect.php");
+            $sql="SELECT * FROM quotation_detail";
+            $result=$conn->query($sql);
+            while($my_row=$result->fetch_assoc()){
+                $QUD_ID=$my_row[QUD_ID];
+                $QI_ID=$my_row[QI_ID];
+                $C_ID=$my_row[C_ID];
+                $QUD_AMOUNT=$my_row[QUD_AMOUNT];
+                $QUD_PRINT=$my_row[QUD_PRINT];
+                $quotation_detailList=new quotation_detail($QUD_ID,$QI_ID,$C_ID,$QUD_AMOUNT,$QUD_PRINT);
+            }
+            require("connection_close.php");
+            return $quotation_detailList;
+        }
         
     }
 ?>

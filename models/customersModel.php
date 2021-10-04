f25fe69b6b87a0c2848b6527a9ab590737122aee
wuttitul <91468323+wuttitul@users.noreply.github.com>
2021-10-04 14:04:33 +0700

    Create customersModel.php

---
+++ models/customersModel.php
@@ -0,0 +1,36 @@
+<?php
+    class customers{
+        public $ID;
+        public $code;
+        public $name;
+        public $address;
+        public $tel;
+
+        public function_construct($ID,$code,$name,$address,$tel){
+            $this->ID=$ID;
+            $this->code=$code;
+            $this->name=$name;
+            $this->address=$address;
+            $this->tel=$tel;
+        }
+
+        
+        public static function getAll(){
+            $customersList=[];
+            require("connection_connect.php");
+            $sql = "SELECT * FROM customers";
+            $result=$conn->query($sql);
+            while($my_row=$result->fetch_assoc())
+            {
+                $ID=$my_row[ID];
+                $code=$my_row[code];
+                $name=$my_row[name];
+                $address=$my_row[address];
+                $tel=$my_row[tel];
+                $customersList[]=new customers($ID,$code,$name,$address,$tel);
+            }
+            require("connection_close.php");
+            return $customersList;
+        }
+    }
+?>

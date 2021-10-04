5408ac07524ab8fc5a92a45acf846e1c3c838af9
wuttitul <91468323+wuttitul@users.noreply.github.com>
2021-10-04 14:29:30 +0700

    Create ordersModel.php

---
+++ models/ordersModel.php
@@ -0,0 +1,11 @@
+<?php
+class orders{
+    public $O_ID:
+    public $O_DATE;
+
+    public function_construct($O_ID,$O_DATE){
+        $this->O_ID=$O_ID;
+        $this->O_DATE=$O_DATE;
+    }
+}
+?>

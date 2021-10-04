750258f81444765e9cbd95fde4f254245a01b282
wuttitul <91468323+wuttitul@users.noreply.github.com>
2021-10-04 14:27:08 +0700

    Create order_detailModel.php

---
+++ models/order_detailModel.php
@@ -0,0 +1,15 @@
+<?php
+class order_detail{
+    public $OD_ID:
+    public $O_ID;
+    public $C_ID;
+    public $O_AMOUNT;
+
+    public function_construct($OD_ID,$O_ID,$C_ID,$O_AMOUNT){
+        $this->OD_ID=$OD_ID;
+        $this->O_ID=$O_ID;
+        $this->C_ID=$C_ID;
+        $this->O_AMOUNT=$O_AMOUNT;
+    }
+}
+?>

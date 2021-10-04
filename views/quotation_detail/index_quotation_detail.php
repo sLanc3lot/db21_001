35f6f65d240900c77c25cb994c13d6c75723ad1f
wuttitul <91468323+wuttitul@users.noreply.github.com>
2021-10-04 14:50:17 +0700

    Create index_quotation_detail.php

---
+++ views/quotation_detail/index_quotation_detail.php
@@ -0,0 +1,23 @@
+<table border =1>
+    <tr> <td>QUD_ID</td>
+    <td>QU_ID</td>
+    <td>C_ID</td>
+    <td>QUD_AMOUNT</td>
+    <td>QUD_PRINT</td>
+    <td>update</td>
+    <td>delete</td> </tr>
+
+<?php
+    foreach($quotation_detailList=$quantation_detail){
+        echo "
+        <tr> <td>quantation_detail->QUD_ID</td>
+        <td>quantation_detail->QU_ID</td>
+        <td>quantation_detail->C_ID</td>
+        <td>quantation_detail->QUD_AMOUNT</td>
+        <td>quantation_detail->QUD_PRINT</td>
+        <td>update</td>
+        <td>delete</td> </tr>
+        ";
+    }
+    echo "</table>";
+?>

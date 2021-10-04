cc37ab188813f929169c4e0b27983ce8d740804d
wuttitul <91468323+wuttitul@users.noreply.github.com>
2021-10-04 14:03:20 +0700

    Update connection_connect.php

--- connection_connect.php
+++ connection_connect.php
@@ -4,6 +4,7 @@ $username = "db21_001";
 $password = "db21_001";
 $dbname = "db21_001";
 $conn = new mysqli($servername,$username,$password);
+mysqli_set_charset($conn, "utf8");
 if ($conn->connect_error) {
     die("Connection failed:" .$conn->connect_error);
 }

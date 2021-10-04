e8bf09c14934035e1aefa66bcb11dfc7c71b3ba8
wuttitul <91468323+wuttitul@users.noreply.github.com>
2021-10-04 14:35:01 +0700

    Create category.php

---
+++ models/category.php
@@ -0,0 +1,11 @@
+<?php
+class category{
+    public $CAT_ID;
+    public $CAT_NAME;
+
+    public function_construct($CAT_ID,$CAT_NAME){
+        $this->CAT_ID=$CAT_ID;
+        $this->CAT_NAME=$CAT_NAME;
+    }
+}
+?>

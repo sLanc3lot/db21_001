6f8eb3b59665ee1c25d84c3fa653eb1d4493703c
wuttitul <91468323+wuttitul@users.noreply.github.com>
2021-10-04 15:51:21 +0700

    Update category.php

--- models/category.php
+++ models/category.php
@@ -7,5 +7,19 @@ class category{
         $this->CAT_ID=$CAT_ID;
         $this->CAT_NAME=$CAT_NAME;
     }
+
+    public function getAll(){
+        $categoryList=[];
+        require("connection_connect.php");
+        $sql="SELECT * FROM category ";
+        $result=$conn->query($sql);
+        while($my_row=$result->fetch_assoc()){
+            $CAT_ID=$my_row[CAT_ID];
+            $CAT_NAME=$my_row[CAT_NAME];
+            $categoryList[]=new category($CAT_ID,$CAT_NAME);
+        }
+        require("connection_close.php");
+        return $categoryList;
+    }
 }
 ?>

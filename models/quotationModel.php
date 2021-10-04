3abf48e580ed5b3f6513c236b284790260b53f59
wuttitul <91468323+wuttitul@users.noreply.github.com>
2021-10-04 14:19:29 +0700

    Create quotationModel.php

---
+++ models/quotationModel.php
@@ -0,0 +1,17 @@
+<?php
+class quotation{
+    public $QI_ID:
+    public $QU_DATE;
+    public $QU_CONDITION;
+    public $EMP_CODE;
+    public $ID;
+
+    public function_construct($QI_ID,$QU_DATE,,$QU_CONDITION,$EMP_CODE,$ID){
+        $this->QI_ID=$QI_ID;
+        $this->QU_DATE=$QU_DATE;
+        $this->QU_CONDITION=$QU_CONDITION;
+        $this->EMP_CODE=$EMP_CODE;
+        $this->ID=$ID;
+    }
+}
+?>

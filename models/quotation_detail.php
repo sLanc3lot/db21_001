70865681fdc4a0eaa9f81ee708e623fdf673d673
wuttitul <91468323+wuttitul@users.noreply.github.com>
2021-10-04 12:04:28 +0700

    Update quotation_detail.php

--- models/quotation_detail.php
+++ models/quotation_detail.php
@@ -1,3 +1,20 @@
 <?php
+    class quotation_detail{
+        public $QUD_ID;
+        public $QI_ID:
+        public $C_ID;
+        public $QUD_AMOUNT;
+        public $QUD_PRINT;
 
+        public function_construct($QUD_ID,$QI_ID,$C_ID,$QUD_AMOUNT,$QUD_PRINT)
+        {
+            $this->QUD_ID=$QUD_ID;
+            $this->QI_ID=$QI_ID;
+            $this->C_ID=$C_ID;
+            $this->QUD_AMOUNT=$QUD_AMOUNT;
+            $this->QUD_PRINT=$QUD_PRINT;
+        }
+
+        
+    }
 ?>
